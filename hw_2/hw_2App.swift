34c850bd50904e819e7a7a17bb775bbca7635ba4
richard <2bloveys@gmail.com>
2023-10-22 19:21:44 +0800

    Initial Commit

---
+++ hw_2/hw_2App.swift
@@ -0,0 +1,17 @@
+//
+//  hw_2App.swift
+//  hw_2
+//
+//  Created by RICHARD on 2023/10/22.
+//
+
+import SwiftUI
+
+@main
+struct hw_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
